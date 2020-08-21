cc471218f74dd463ca23f253fa87420db8588981
Nastik1811 <lugovskaya.1999@mail.ru>
2020-08-21 16:09:52 +0300

    Add hero and content sections

--- src/views/Projects/index.js
+++ src/views/Projects/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import projects from '../../data/projects';
 
 const Projects = () => {
   return (
     <div className="main-container">
+      <section className="hero">
+        <h1 className="hero-title">Projects</h1>
+        <p className="hero-text"> 
+          From configuration to security, web apps to big data—whatever the infrastructure needs of your application may be, there is a Spring Project to help you build it. Start small and use just what you need—Spring is modular by design.
+        </p>
+      </section>
+      <div className="content">
+        <div className="projects-list">
+          {projects.map(p => <div>{p.name}</div>)}
+        </div>
+      </div>
     </div>
   );
 }
