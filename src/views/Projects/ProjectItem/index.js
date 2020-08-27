863ebb121b1a5882e71af01934af404bfd8aeaee
Nastik1811 <lugovskaya.1999@mail.ru>
2020-08-27 10:59:01 +0300

    Add responsive project items

--- src/views/Projects/ProjectItem/index.js
+++ src/views/Projects/ProjectItem/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import icon from '../../../assets/images/spring-boot.svg';
 
 const ProjectItem = ({project}) => {
     return(
         <a href={project.url} className="project-item">
                 <div className="project-icon">
+                    <img className="icon" src={icon} alt="icon" />
                 </div>
                 <section className="project-info">
                     <h3>{project.name}</h3>
