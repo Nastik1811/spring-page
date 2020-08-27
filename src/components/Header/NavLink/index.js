2ab1330ed4431e2a1dd856029507f4df8beab1e3
Nastik1811 <lugovskaya.1999@mail.ru>
2020-08-27 10:36:47 +0300

    Add adaptive navigation

--- src/components/Header/NavLink/index.js
+++ src/components/Header/NavLink/index.js
@@ -1,39 +1,90 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+
+const Dropdown = ({links}) => {
+    return(
+        <div className="drop-menu">
+            {
+                links.map(
+                    link => 
+                    <div className="sublink">
+                        <a href={link.url} className="nav-link">
+                            {link.title}
+                        </a>
+                    </div>
+                    )
+                }
+        </div>
+    )
+}
 
 const NavLink = ({link}) => {
     if(link.sublinks){ 
         return(
-            <li className="dropdown">
-                <a href={link.url} className="nav-link">
+            <li className="nav-item">
+                <span>
                     {link.title}
                     <div className="drop-arrow"></div>
-                </a>
-                <div className="drop-menu">
-                    <ul className="sublinks">
-                        {
-                            link.sublinks.map(
-                            subl => 
-                                <li>
-                                    <a href="#" className="nav-link sublink">
-                                        {subl.title}
-                                    </a>
-                                </li>
-                            )
-                        }
-                </ul>
-                </div>
-                
+                </span>
+                <Dropdown links={link.sublinks}/>
             </li>
         )
     }
     return(
-        <li>
-            <a href={link.url} className="nav-link">
+        <li className="nav-item">
+            <a className="nav-link" href={link.url} >
                 {link.title}
             </a>
         </li>
     )
 }
+
+export const Sidepane = ({links, expaned, onClose}) => {
+    return(
+        <div className={expaned ? "overlay" : "overlay hidden"}>
+            <button className="close-btn" onClick={onClose}>Hide</button>
+            <nav className="nav-box">
+                {links.map(link => <MenuLink link={link}/>)}
+            </nav>
+        </div>
+    )
+}
+
+const MenuLink = ({link}) => {
+    const [open, setOpen] = useState(false)
+
+    if(link.sublinks){
+        return(
+            <div className="container">
+                <div className="menu-item" onClick={() => setOpen(!open)}>
+                    <span >
+                        {link.title}
+                    </span>
+                    <div className={open ? "drop-arrow expaned" : "drop-arrow"}></div>
+                </div>
+                <div className={open ? "submenu" : "submenu hidden"}>
+                        {link.sublinks.map(l =>
+                            <div className="sub-item">
+                                <a href={l.url} >
+                                    {l.title}
+                                </a>
+                            </div>
+                        )}
+                </div>
+            </div>
+        )
+    }
+
+    return(
+        <div className="menu-item">
+            <a href={link.url} >
+                {link.title}
+            </a>
+        </div>
+    )
+
+}
+
+
 
 export default NavLink
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/images/spring-logo.svg';
 import navLinks from '../../data/navLinks';
-import NavLink from './NavLink';
+import NavLink, { Sidepane } from './NavLink';
+import './index.css';
 
+
+
+const Navbar = ({links}) => {
+    return(
+        <ul className="desktop-nav">
+            {links.map(link => <NavLink link={link} key={link.title}/>)}
+        </ul>
+    )
+}
 const Header = () => {
-  return (
-    <header className="main-header">
-        <div className="nav-container">
-            <a href="/" id="logo-focus" tabindex="1">
-                <img alt="Spring" class="block" id="springlogo" src={logo}/>
-            </a>
-            <nav className="main-nav">
-                <ul className="nav-links">
-                    {navLinks.map(link => <NavLink link={link}/>)}
-                </ul>
+    const [mobileExpaned, setMobileExpaned] = useState(false)
+    
+    return(
+         <header className="app-header">
+            <nav className="nav-container">
+                <a href="/">
+                    <img alt="Spring" id="springlogo" src={logo}/>
+                </a>
+                <Navbar links={navLinks}/>
+                <button className="open-btn" onClick={() => setMobileExpaned(true)}></button>
             </nav>
-        </div>
-    </header>
-  );
+           <Sidepane links={navLinks} expaned={mobileExpaned} onClose={() => setMobileExpaned(false)}/>
+        </header>
+    )
 }
 export default Header
